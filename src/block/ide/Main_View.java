5f70599c3a20f2b87565801bb2657d89770a6b0d
T.Akiyama <t.aki0616@gmail.com>
2016-04-29 00:48:18 +0900

    1604290048

--- src/block/ide/Main_View.java
+++ src/block/ide/Main_View.java
@@ -1,24 +1,41 @@
 package block.ide;
 
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.*;
+import java.util.Locale;
+import block.ide.lang.MsgTrans;
 
 public class Main_View extends JApplet{
 	private static final long serialVersionUID = 1L;
 	public void init(){
-		JTabbedPane tabpane = new JTabbedPane();
+//		Locale defaultLocale = Locale.getDefault();
+		Locale.setDefault(Locale.US);
+		resize(800,600);
+		Dimension dim = getSize();
+		Integer pane_wide = (dim.width-20)/13;
+		Integer pane_heig = (dim.height-20)/13;
+			JTabbedPane tabpane = new JTabbedPane();
 		JPanel tab1 = new JPanel();
-		tab1.add(new JButton("Button1"));
+		JPanel parts_win = new JPanel();
+		parts_win.setPreferredSize(new Dimension(pane_wide*5,pane_heig*10));
+		parts_win.setBorder(new LineBorder(Color.red,2));
+		JPanel main_win = new JPanel();
+		main_win.setPreferredSize(new Dimension(pane_wide*8,pane_heig*10));
+		main_win.setBorder(new LineBorder(Color.red,2));
+		tab1.add(parts_win);
+		tab1.add(main_win);
+		
 		JPanel tab2 = new JPanel();
 		tab2.add(new JButton("Button2"));
 		JPanel tab3 = new JPanel();
 		tab3.add(new JButton("Button3"));
 		JPanel tab4 = new JPanel();
 		tab4.add(new JLabel("Infomation"));
-		tabpane.addTab("Block",tab1);
-		tabpane.addTab("Text",tab2);
-		tabpane.addTab("Config", tab3);
-		tabpane.addTab("Help", tab4);
+		tabpane.addTab(MsgTrans.TABNAME01.toString(),tab1);
+		tabpane.addTab(MsgTrans.TABNAME02.toString(),tab2);
+		tabpane.addTab(MsgTrans.TABNAME03.toString(), tab3);
+		tabpane.addTab(MsgTrans.TABNAME04.toString(), tab4);
 		
 		Container contentPane = getContentPane();
 		contentPane.add(tabpane, BorderLayout.CENTER);
---
+++ src/block/ide/lang/MsgTrans.java
@@ -0,0 +1,20 @@
+package block.ide.lang;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+	
+public enum MsgTrans {
+	TABNAME01,
+	TABNAME02,
+	TABNAME03,
+	TABNAME04;
+	
+	@Override public String toString(){
+		try{
+			return ResourceBundle.getBundle("block.ide.lang.MSG",Locale.getDefault()).getString(name());
+		}catch(Exception ex){
+			ex.printStackTrace();
+			return null;
+		}
+	}
+}
