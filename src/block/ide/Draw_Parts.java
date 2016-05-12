8286f0d69fd90f0b2a9658e2fb8f1ac359f2af72
T.Akiyama <t.aki0616@gmail.com>
2016-05-12 13:18:07 +0900

    201605111318

--- src/block/ide/Draw_Parts.java
+++ src/block/ide/Draw_Parts.java
@@ -3,27 +3,37 @@ package block.ide;
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 public class Draw_Parts extends Panel implements MouseListener,MouseMotionListener{
 	private static final long serialVersionUID = 1L;
 	//部品描画クラス
 	int x1,y1;
-	public JScrollPane DrawPanel(Menu_Icon[] lists){
+	public JScrollPane DrawPanel(Menu_Icon[] lists,int pane_wide,int pane_heig){
 		JScrollPane pane = new JScrollPane();
-		setForeground(Color.red);
-		setBackground(Color.white);
-		//部品を保管するLabelを作成。とりあえず、部品の数は１００個
-		//今後、XMLの要素数になるように変更する。
+//		pane.setLayout(new FlowLayout());
+		pane.setPreferredSize(new Dimension(pane_wide*5,pane_heig*13));
+		pane.setBorder(new LineBorder(Color.green,2));
+		JViewport jview = pane.getViewport();
+
+		//部品の数を抽出
 		int i = 0;
 		while(lists[i].id != 0){
 			i++;
 		}
-		System.out.println(i);
-		JLabel[] label = new JLabel[100];
+		JTextField text = new JTextField();
+		JLabel label1 = new JLabel("TEST");
+		jview.add(label1);
+		jview.add(text);
+		JLabel[] label = new JLabel[i];
+		for(int j = 0;j < i;j++){
+			label[j] = new JLabel();
+			label[j].setText(lists[j].name);
+//			pane.add(label[j]);			
+		}
 		
-		
-		addMouseMotionListener(this);
-		addMouseListener(this);
+//		addMouseMotionListener(this);
+//		addMouseListener(this);
 		return pane;
 	}
 	
--- src/block/ide/Main_View.java
+++ src/block/ide/Main_View.java
@@ -69,12 +69,13 @@ public class Main_View extends JApplet{
 	public JPanel tab1_view(Integer pane_wide,Integer pane_heig,JPanel tab1,Menu_Icon[] lists){
 		//JScrollPane parts_win = new JScrollPane();
 		Draw_Parts dpf = new Draw_Parts();
-		JScrollPane parts_win = dpf.DrawPanel(lists);
-		parts_win.setPreferredSize(new Dimension(pane_wide*5,pane_heig*13));
-		parts_win.setBorder(new LineBorder(Color.red,2));
+		JScrollPane parts_win = dpf.DrawPanel(lists,pane_wide,pane_heig);
+		JLabel label1 = new JLabel("test");
+		parts_win.add(label1);
 		JScrollPane main_win = new JScrollPane();
 		main_win.setPreferredSize(new Dimension(pane_wide*8,pane_heig*13));
-		main_win.setBorder(new LineBorder(Color.red,2));
+		main_win.setBorder(new LineBorder(Color.blue,2));
+		main_win.add(label1);
 		tab1.add(parts_win);
 		tab1.add(main_win);
 		return tab1;
