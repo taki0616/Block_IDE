4777a18124ea67c1fc5a84021d22e5a4449cbdff
T.Akiyama <t.aki0616@gmail.com>
2016-05-16 13:02:19 +0900

    201605161302

--- src/block/ide/Draw_Parts.java
+++ src/block/ide/Draw_Parts.java
@@ -10,30 +10,38 @@ public class Draw_Parts extends Panel implements MouseListener,MouseMotionListen
 	//部品描画クラス
 	int x1,y1;
 	public JScrollPane DrawPanel(Menu_Icon[] lists,int pane_wide,int pane_heig){
-		JScrollPane pane = new JScrollPane();
-//		pane.setLayout(new FlowLayout());
+		JScrollPane pane = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		pane.setPreferredSize(new Dimension(pane_wide*5,pane_heig*13));
 		pane.setBorder(new LineBorder(Color.green,2));
 		JViewport jview = pane.getViewport();
+		JPanel panels = new JPanel();
+		panels.setLayout(null);
+		panels.setBackground(Color.white);
 
 		//部品の数を抽出
 		int i = 0;
 		while(lists[i].id != 0){
 			i++;
 		}
-		JTextField text = new JTextField();
-		JLabel label1 = new JLabel("TEST");
-		jview.add(label1);
-		jview.add(text);
 		JLabel[] label = new JLabel[i];
+		//ラベルをグルーピングするか？
+		//グループするならlists[j].shapeで判断する
+		//(int)(shape/100)
+		//1:Command 2:Input 3:Output 4: 5:Other 
 		for(int j = 0;j < i;j++){
 			label[j] = new JLabel();
 			label[j].setText(lists[j].name);
-//			pane.add(label[j]);			
+			label[j].setBounds(5,j*50+j*5+5,100,50);
+			label[j].setHorizontalAlignment(JLabel.CENTER);
+			label[j].setVerticalAlignment(JLabel.CENTER);			
+			label[j].setBorder(new LineBorder(Color.black,2));
+			panels.add(label[j]);
+			System.out.println(lists[j].code);
 		}
+		jview.add(panels);
 		
-//		addMouseMotionListener(this);
-//		addMouseListener(this);
+		addMouseMotionListener(this);
+		addMouseListener(this);
 		return pane;
 	}
 	
